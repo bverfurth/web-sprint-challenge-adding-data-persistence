c9dffee389c1a064e8189a09558435d42bd87a91
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-10 16:10:47 -0400

    re did task files. got last test to pass! :')

--- api/task/router.js
+++ api/task/router.js
@@ -3,22 +3,31 @@ const Tasks = require("./model");
 
 const router = express.Router();
 
+// [GET] all tasks
 router.get("/", (req, res, next) => {
-  Tasks.getTasks()
+  Tasks.find()
     .then((tasks) => {
-      res.json(tasks);
+      res.status(200).json(trueOrFalse(tasks));
     })
     .catch(next);
 });
 
+// [POST] a task
 router.post("/", (req, res, next) => {
-  const task = req.body;
-
-  Tasks.addTask(task)
+  Tasks.add(req.body)
     .then((task) => {
-      res.status(201).json(task);
+      //task is an array of one task
+      res.status(201).json(trueOrFalse(task)[0]);
     })
     .catch(next);
 });
+
+// function to convert integer to boolean
+const trueOrFalse = (tasks) => {
+  return tasks.map((proj) => ({
+    ...proj,
+    task_completed: proj.task_completed ? true : false,
+  }));
+};
 
 module.exports = router;
