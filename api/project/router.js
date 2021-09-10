69a0f9741f108ce7e74c0f046bb65b07d488a840
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-10 15:15:41 -0400

    projects router complete and passing

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,40 @@
-// build your `/api/projects` router here
+const express = require("express");
+const Projects = require("./model");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await Projects.getAll();
+    const convertedProjects = projects.map((project) => {
+      return {
+        ...project,
+        project_completed: Boolean(project.project_completed),
+      };
+    });
+    res.status(200).json(convertedProjects);
+  } catch (err) {
+    next();
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    if (!req.body.project_name) {
+      next({
+        status: 400,
+        message: "Project name is required",
+      });
+    } else {
+      const newProject = await Projects.insert(req.body);
+      res.status(201).json({
+        ...newProject,
+        project_completed: Boolean(newProject.project_completed),
+      });
+    }
+  } catch (err) {
+    next();
+  }
+});
+
+module.exports = router;
