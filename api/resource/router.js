7f9773f7f7a2fc0eb2b1f6aa5b82d275617ee16d
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-10 15:20:59 -0400

    router resource done

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,35 @@
-// build your `/api/resources` router here
+const express = require("express");
+const Resources = require("./model");
+const db = require("./../../data/dbConfig");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const resources = await Resources.getAll();
+    res.status(200).json(resources);
+  } catch (err) {
+    next();
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const existingName = await db("resources")
+      .where("resource_name", req.body.resource_name)
+      .first();
+    if (!existingName) {
+      const newResource = await Resources.insert(req.body);
+      res.status(201).json(newResource);
+    } else {
+      next({
+        status: 400,
+        message: "Resource name is already taken",
+      });
+    }
+  } catch (err) {
+    next();
+  }
+});
+
+module.exports = router;
