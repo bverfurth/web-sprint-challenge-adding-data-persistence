3d6f9d46a1017fe1fb42b4b3f11f74a5b596ad09
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-10 14:49:13 -0400

    server.js looks good

--- api/server.js
+++ api/server.js
@@ -1,1 +1,27 @@
-// build your server here and require it from index.js
+const express = require("express");
+const server = express();
+const resourceRouter = require("./resource/router");
+const projectRouter = require("./project/router");
+const taskRouter = require("./task/router");
+
+server.use(express.json());
+server.use("/api/resources", resourceRouter);
+server.use("/api/projects", projectRouter);
+server.use("/api/tasks", taskRouter);
+
+// eslint-disable-next-line
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    custom: "Something went wrong with a router.",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+server.use("*", (req, res) => {
+  res.json({
+    message: "Something went wrong here! :(",
+  });
+});
+
+module.exports = server;
